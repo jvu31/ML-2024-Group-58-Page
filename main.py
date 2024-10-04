ce06b65b661b029b06a92756052b540783e639ff
Shrenik Koppaka <shrenikop@gmail.com>
2024-10-04 16:20:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ st.set_page_config(page_title='Star Classification Model', page_icon ="🌟", la
 st.title('Star Classification Model')
 st.header('CS 4641 - Fall 2024 - Group 58')
 
-st.subheader("[Project Github](https://github.com/Skoppaka9/ML-2024-Group-58)")
+st.header("[Project Github](https://github.com/Skoppaka9/ML-2024-Group-58)")
 
-st.subheader('Introduction/Background')
+st.header('Introduction/Background')
 st.write(":heavy_minus_sign:"*14)
 st.write("Stellar classification is used by scientists to categorize stars based on their spectral characteristics (temperature, size, composition, color, brightness, and more). By classifying these stars, scientists can better understand them by analyzing their patterns and trends, which will hopefully provide us with more knowledge and insight into the universe.")
+st.subheader('Literature Review')
+st.write(":heavy_minus_sign:"*14)
 st.write("""
 - Armstrong et al., “K2 variable catalogue – II. Machine learning classification of variable stars and eclipsing binaries in K2 fields 0–4”
 Armstrong et al. utilize a combination of Kohonen Self-Organizing Maps (SOMs an unsupervised machine learning algorithm) and Random Forest as a new method for variable star classification [1]. SOMs were used to effectively parameterize light curve shapes while Random Forest were useful for their classification schemes especially for larger data sets [1].   
@@ -33,6 +35,7 @@ This paper focuses on improving Guide Star Catalogues (GSCs) used in star identi
 Qi uses three machine learning algorithms, Decision Tree, Random Forest and Support Vector Machine, to build prediction models for stellar classification [3]. The results show that random forest reaches highest accuracy with greatest computing efficiency [3]. 
 
 """)
+st.subheader('Dataset')
 st.write("""
 This [Star Dataset](https://www.kaggle.com/datasets/vinesmsuic/star-categorization-giants-and-dwarfs?select=Star9999_raw.csv) uses spectral data to distinguish whether a star is a dwarf or giant. 
 
@@ -42,25 +45,27 @@ This [Star Dataset](https://www.kaggle.com/datasets/vinesmsuic/star-categorizati
 
 
 
-st.subheader('Problem Definition')
-st.write("-"*11)
+st.header('Problem Definition')
+st.write(":heavy_minus_sign:"*11)
 st.write("Given that technology is advancing, more data and features of stellar bodies will become available. Instead of manually categorizing the data, we can automate the process and make it more consistent and efficient with machine learning methods. The problem is a binary classification: either a dwarf or giant star. Using various ML models related to binary classification, we will categorize the stars based on the relevant features in the dataset.")
 
-st.subheader('Methods')
+st.header('Methods')
+st.write(":heavy_minus_sign:"*6)
 st.write("""
 Preprocessing methods:
-- Dimensionality Reduction (PCA): relevant for identifying core features that have the biggest impact on classification
-- Data Normalization (StandardScalar): needed given that features can range from small to large ranges
-- Data Transformation: encoding categorical data into numeric values
+- Data Cleaning: Check for missing values/empty rows, remove duplicate rows, remove rows with high standard error, formatting, and splitting data into training and testing sets
+- Data Normalization (StandardScalar): Needed given that features can range from small to large value ranges; improves model's performance, accuracy, and stability
+- Data Transformation: Encoding categorical data into numeric values, one-hot encoding because spectral types have roman numerals
     """)
 st.write(""" 
 ML Models: 
-- HDBScan: model that is able to scale with large datasets
-- KNN: good classification model for low dimensionality dataset
-- SVM: optimal for binary classification
+- Random Forest: Effective for star classification problems as can be seen in the literature review, import RandomForestClassifier from sklearn.ensemble
+- KNN: A simple and good classification model for a low dimensionality, well-defined dataset, import KNeighborsClassifier from sklearn.neighbors
+- Logistic Regression: Useful for binary classification as it predicts whether a data point belongs to a category, import LogisticRegression from sklearn.linear_model        
 """)
 
-st.subheader('(Potential) Results and Discussion')
+st.header('(Potential) Results and Discussion')
+st.write(":heavy_minus_sign:"*17)
 st.write("""
 We plan to use accuracy, precision, and recall as our metrics to evaluate our model.
 
@@ -76,7 +81,7 @@ Each metric is expected to be at least 85% for our models, and we expect the ran
 
 """)
 
-st.subheader('References')
+st.header('References')
 st.write("""
 [1] D. G. Armstrong et al., “K2 variable catalogue – II. Machine learning classification of variable stars and eclipsing binaries in K2 fields 0–4,” Monthly Notices of the Royal Astronomical Society, vol. 456, no. 2, pp. 2260–2272, Feb. 2016, doi: https://doi.org/10.1093/mnras/stv2836. 
 
@@ -86,10 +91,10 @@ st.write("""
 
 """)
 
-st.subheader('Gantt Chart')
-st.subheader('Contribution Table')
+st.header('Gantt Chart')
+st.header('Contribution Table')
 st.data_editor(contributions, hide_index=True)
 st.divider()
-st.caption("Created by Jimmy Vu, Shrenik Koppaka, Carlos Aponte, Omikhosen Unuigboje, Jerry Song | © 2024 Star Classification Project")
+st.caption("Jimmy Vu, Shrenik Koppaka, Carlos Aponte, Omikhosen Unuigboje, Jerry Song | © 2024 Star Classification Project")
 
 
